e542fc22af2c09d241bb9ca49dd6cbc33da9d930
Satya Kresna <satyakresna6295@gmail.com>
2019-12-06 15:49:16 +0800

    feat: store unit list in json file

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const fs = require("fs");
+const chalk = require("chalk");
+
+const units = [];
+const outputFile = 'units.json';
 
 request("https://bravefrontierglobal.fandom.com/wiki/Unit_List", (error, response, body) => {
   if (!error && response.statusCode == 200) {
@@ -9,26 +14,46 @@ request("https://bravefrontierglobal.fandom.com/wiki/Unit_List", (error, respons
       const rows = $(el).find($("tr"));
       rows.each((i, el) => {
         const columns = $(el).find($("td"));
-        columns.each((i, el) => {
+        let name, link, thumbnail, element, rarity, cost;
+        columns.map((i, el) => {
           switch (i) {
             case 1:
-              console.log("unit image", $(el).find("a > img").attr("data-src"));
-              console.log("unit name", $(el).find("a").last().attr("title"));
-              console.log("unit link", $(el).find("a").last().attr("href"))
+              if (typeof $(el).find("a > img").attr("data-src") !== "undefined") {
+                thumbnail = $(el).find("a > img").attr("data-src");
+              } else {
+                thumbnail = $(el).find("a > img").attr("src");
+              }
+              name = $(el).find("a").last().attr("title");
+              link = $(el).find("a").last().attr("href");
               break;
             case 2:
-              console.log("unit element", $(el).find("center > a").attr("title"));
+              element = $(el).find("center > a").attr("title");
               break;
             case 3:
-              console.log("unit rarity", $(el).find("center > a").attr("title"));
+              rarity = $(el).find("center > a").attr("title");
               break;
             case 4:
-              console.log("unit cost", $(el).find("center > a").attr("title"));
+              cost = $(el).find("center > a").attr("title");
               break;
           }
         });
+        units.push({
+          name, link, thumbnail, element, rarity, cost
+        });
       });
     });
+
+    // Remove first element
+    units.shift();
+    console.log(units);
+    console.log(units.length);
+
+    fs.writeFile(outputFile, JSON.stringify(units, null, 4), (err) => {
+      if (err) {
+        console.log(err)
+      }
+      console.log(chalk.yellow.bgBlue(`\n ${chalk.underline.bold(units.length)} Results exported successfully to ${chalk.underline.bold(outputFile)}\n`));
+    })
   } else {
     console.log(error);
   }
