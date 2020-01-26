2f9cea5499ce066b79a10a908fc00aabcd762d64
Satya Kresna <satyakresna6295@gmail.com>
2020-01-27 06:59:41 +0800

    refactor: explicit measure time

--- scrape-units.js
+++ scrape-units.js
@@ -176,7 +176,7 @@ async function collectUnits() {
 function millisToMinutesAndSeconds(millis) {
   var minutes = Math.floor(millis / 60000);
   var seconds = ((millis % 60000) / 1000).toFixed(0);
-  return (seconds == 60 ? (minutes + 1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds);
+  return `${ (seconds == 60 ? (minutes + 1) : minutes) } ${ (minutes > 1) ? "minutes" : "minute" } and ${ (seconds < 10 ? "0" : seconds) } ${ (seconds > 1 ? "seconds" : "second") }`;
 }
 
 const getUnitBio = (unitLink) => {
