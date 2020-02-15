5f00e47d84af406e22098bdfa5a4287bb531d690
Satya Kresna <satyakresna6295@gmail.com>
2020-02-15 18:21:28 +0800

    style: formatting result

--- scrape/omni-units.js
+++ scrape/omni-units.js
@@ -115,7 +115,7 @@ const updateOmniUnits = async () => {
 function millisToMinutesAndSeconds(millis) {
   var minutes = Math.floor(millis / 60000);
   var seconds = ((millis % 60000) / 1000).toFixed(0);
-  return `${ (seconds == 60 ? (minutes + 1) : minutes) } ${ (minutes > 1) ? "minutes" : "minute" } and ${ (seconds < 10 ? "0" : seconds) } ${ (seconds > 1 ? "seconds" : "second") }`;
+  return `${(seconds == 60 ? (minutes + 1) : minutes)} ${(minutes > 1) ? "minutes" : "minute"} and ${(seconds < 10 ? "0" : seconds)} ${(seconds > 1 ? "seconds" : "second")}`;
 }
 
 updateOmniUnits();
