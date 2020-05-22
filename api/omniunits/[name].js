35d5b8585893ebdf751e1353d96b33b0e899bd90
Satya Kresna <satyakresna6295@gmail.com>
2020-05-22 19:56:15 +0800

    feat(api): find unit with dynamic value by name

---
+++ api/omniunits/[name].js
@@ -0,0 +1,25 @@
+const fsPromises = require('fs').promises;
+const { join } = require('path');
+const file = join(__dirname, '..', '..', 'src', 'omniunits', 'data.json');
+
+module.exports = async (req, res) => {
+    const {
+        query: { name }
+    } = req;
+
+    const text = await fsPromises.readFile(file, 'utf-8');
+    const omniUnits = JSON.parse(text);
+    let selectedUnit = {};
+    for (let omniUnit of omniUnits) {
+        if (omniUnit.name === name.split('_').join(' ').trim()) {
+            selectedUnit = omniUnit;
+        }
+    }
+
+    const statusCode = (selectedUnit.hasOwnProperty('name')) ? 200 : 404;
+    const result = (selectedUnit.hasOwnProperty('name')) 
+        ? selectedUnit 
+        : { message : `Unit ${name} not found` };
+
+    res.status(statusCode).send(result);
+}
