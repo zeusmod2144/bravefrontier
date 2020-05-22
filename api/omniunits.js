ad3b1f8085ef991406f2dd8654bc0e0adc6aad5c
Satya Kresna <satyakresna6295@gmail.com>
2020-05-22 17:43:53 +0800

    feat(api): get list of omni units

---
+++ api/omniunits.js
@@ -0,0 +1,14 @@
+const fsPromises = require('fs').promises;
+const { join } = require('path');
+const file = join(__dirname, '..', 'src', 'omniunits', 'data.json');
+
+module.exports = async (req, res) => {
+    const text = await fsPromises.readFile(file, 'utf8');
+    const omniUnits = JSON.parse(text);
+    for (const omniUnit of omniUnits) {
+        delete omniUnit.id;
+        delete omniUnit.artwork;
+        delete omniUnit.spRecommendation;
+    }
+    res.status(200).send(omniUnits);
+}
