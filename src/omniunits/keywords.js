f89c451e1032fee326014a49a5aa8e82bff27ade
Kresna <satyakresna6295@gmail.com>
2020-09-13 08:56:19 +0800

    fix(omniunits/keywords): negates all status ailments for all allies

--- src/omniunits/keywords.js
+++ src/omniunits/keywords.js
@@ -1,7 +1,7 @@
 module.exports = [
     "extra action", "evasion", "activates BB/SBB/UBB twice",
     "DoT mitigation", "negate all status ailments",
-    "negates all status ailments",
+    "negates all status ailments for all allies",
     "Stealth", "normal attacks may hit all foes",
     "raises normal hit amount", "status ailment removal",
     "def ignoring effect",
