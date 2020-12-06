f32682ba58627700d31e2af9c293c83325a452be
Kresna <satyakresna6295@gmail.com>
2020-12-06 13:16:48 +0800

    new mechanism to download artworks and thumbnails of omniunits

--- src/omniunits/artworks.js
+++ src/omniunits/artworks.js
@@ -1,23 +1,17 @@
 const axios = require('axios');
 const fs = require('fs');
 const fsPromises = fs.promises;
-const sharp = require('sharp');
 const { join } = require('path');
-const imagemin = require('imagemin');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminWebp = require('imagemin-webp');
-const del = require('del');
 
-const omniUnitsFile = join(__dirname, 'raw.json');
-const { bytesToSize } = require('../helper.js');
+const omniUnitsFile = join(__dirname, '..', '..', 'data', 'omniunits', 'raw.json');
 
 const downloadFile = (link) => {
     return new Promise((resolve, reject) => {
         axios.get(link, {
-            responseType: 'arraybuffer'
+            responseType: 'stream'
         }).then(response => {
-                return resolve(response)
-            })
+            return resolve(response.data)
+        })
             .catch(error => {
                 return reject(error);
             })
@@ -26,43 +20,18 @@ const downloadFile = (link) => {
 
 (async () => {
     try {
-        fs.mkdir('src/omniunits/tmp/artworks', { recursive: true }, (err) => {
+        fs.mkdir(join(__dirname, '..', '..', 'tmp', 'omniunits', 'artworks'), { recursive: true }, (err) => {
             if (err) throw err;
         });
         const text = await fsPromises.readFile(omniUnitsFile, 'utf8');
         const omniUnits = JSON.parse(text);
         for (const omniUnit of omniUnits) {
-            const response = await downloadFile(omniUnit.artwork);
-            const data = await response.data;
-            const resizeFileByHalf = await sharp(data)
-            .metadata()
-            .then(({ width, height }) => sharp(data)
-              .resize(Math.round(width * 0.3), Math.round(height * 0.3))
-              .png()
-              .toFile(`src/omniunits/tmp/artworks/${omniUnit.id}.png`)
-            );
-            console.log(`${omniUnit.id}. ${omniUnit.name} downloaded. Size: ${bytesToSize(resizeFileByHalf.size)}`);
+            await downloadFile(omniUnit.artwork)
+                .then(data => {
+                    data.pipe(fs.createWriteStream(join(__dirname, '..', '..', 'tmp', 'omniunits', 'artworks', `${omniUnit.id}.png`)));
+                    console.log(`Success download thumbnails of ${omniUnit.name}`);
+                });
         }
-
-        // Reduce png size with pngquant
-        await imagemin(['src/omniunits/tmp/artworks/*.png'], {
-            destination: 'src/omniunits/artworks',
-            plugins: [
-                imageminPngquant()
-            ]
-        });
-
-        // Convert to webp
-        await imagemin(['src/omniunits/artworks/*.png'], {
-            destination: 'src/omniunits/artworks',
-            plugins: [
-                imageminWebp({quality: 50})
-            ]
-        });
-
-        console.log('Compression artwork units success!');
-
-        await del('src/omniunits/tmp');
     } catch (error) {
         console.log(error);
     }
--- src/omniunits/thumbnails.js
+++ src/omniunits/thumbnails.js
@@ -1,22 +1,17 @@
 const axios = require('axios');
 const fs = require('fs');
 const fsPromises = fs.promises;
-const sharp = require('sharp');
 const { join } = require('path');
-const imagemin = require('imagemin');
-const imageminWebp = require('imagemin-webp');
-const del = require('del');
 
-const omniUnitsFile = join(__dirname, 'raw.json');
-const { bytesToSize } = require('../helper.js');
+const omniUnitsFile = join(__dirname, '..', '..', 'data', 'omniunits', 'raw.json');
 
 const downloadFile = (link) => {
     return new Promise((resolve, reject) => {
         axios.get(link, {
-            responseType: 'arraybuffer'
+            responseType: 'stream'
         }).then(response => {
-                return resolve(response)
-            })
+            return resolve(response.data)
+        })
             .catch(error => {
                 return reject(error);
             })
@@ -25,35 +20,18 @@ const downloadFile = (link) => {
 
 (async () => {
     try {
-        fs.mkdir('src/omniunits/tmp/thumbnails', { recursive: true }, (err) => {
+        fs.mkdir(join(__dirname, '..', '..', 'tmp', 'omniunits', 'thumbnails'), { recursive: true }, (err) => {
             if (err) throw err;
         });
         const text = await fsPromises.readFile(omniUnitsFile, 'utf8');
         const omniUnits = JSON.parse(text);
         for (const omniUnit of omniUnits) {
-            const response = await downloadFile(omniUnit.thumbnail);
-            const data = await response.data;
-            const resizeFileByHalf = await sharp(data)
-            .metadata()
-            .then(({ width, height }) => sharp(data)
-              .resize(Math.round(width * 0.5), Math.round(height * 0.5))
-              .png()
-              .toFile(`src/omniunits/thumbnails/${omniUnit.id}.png`)
-            );
-            console.log(`${omniUnit.id}. ${omniUnit.name} downloaded. Size: ${bytesToSize(resizeFileByHalf.size)}`);
+            await downloadFile(omniUnit.thumbnail)
+                .then(data => {
+                    data.pipe(fs.createWriteStream(join(__dirname, '..', '..', 'tmp', 'omniunits', 'thumbnails', `${omniUnit.id}.png`)));
+                    console.log(`Success download thumbnails of ${omniUnit.name}`);
+                });
         }
-
-        // Convert to webp
-        await imagemin(['src/omniunits/thumbnails/*.png'], {
-            destination: 'src/omniunits/thumbnails',
-            plugins: [
-                imageminWebp({quality: 50})
-            ]
-        });
-
-        console.log('Compression thumbnail units success!');
-
-        await del('src/omniunits/tmp');
     } catch (error) {
         console.log(error);
     }
